692ee75446d2c9ca227b25761fe3deeb1ee844cb
faro-tech <farotech.co@gmail.com>
2022-11-21 12:11:49 +0800

    initial

--- main.js
+++ main.js
@@ -1,6 +1,61 @@
 const innerContainer = document.querySelector(".inner-container");
+const nameContainer = document.querySelector(".name-container");
+const aboutContainer = document.querySelector(".about-container");
+const contactContainer = document.querySelector(".contact-container");
+const emailContainer = document.querySelector(".email-container");
 
 var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+const aboutBtn = document.getElementById("about");
+const introductionBtn = document.getElementById("introduction");
+const teamBtn = document.getElementById("team");
+const contactBtn = document.getElementById("contact");
+const backTeamBtn = document.getElementById("back-team");
+const backAboutBtn = document.getElementById("back-about");
+const backIntroductionBtn = document.getElementById("back-introduction");
+const backContactBtn = document.getElementById("back-contact");
+
+const navigate = (target, direction = false) => {
+  if (direction) {
+    target.classList.remove("hover");
+    target.classList.add("unhover");
+    setTimeout(() => {
+      emailContainer.classList.remove("unhover");
+      emailContainer.classList.add("hoverEmail");
+    }, 1000);
+    return;
+  }
+  emailContainer.classList.remove("hoverEmail");
+  emailContainer.classList.add("unhover");
+  setTimeout(() => {
+    target.classList.remove("unhover");
+    target.classList.add("hover");
+  }, 1000);
+};
+
+teamBtn.addEventListener("click", (e) => {
+  navigate(nameContainer);
+});
+
+aboutBtn.addEventListener("click", (e) => {
+  navigate(aboutContainer);
+});
+
+contactBtn.addEventListener("click", (e) => {
+  navigate(contactContainer);
+});
+
+backTeamBtn.addEventListener("click", (e) => {
+  navigate(nameContainer, true);
+});
+
+backAboutBtn.addEventListener("click", (e) => {
+  navigate(aboutContainer, true);
+});
+
+backContactBtn.addEventListener("click", (e) => {
+  navigate(contactContainer, true);
+});
 
 if (isSafari) {
     document.addEventListener("click", e => {
